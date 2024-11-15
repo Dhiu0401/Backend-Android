361cf4043e591778fe61140bb0f3f8f747797ccb
Dhiu0401 <165197217+Dhiu0401@users.noreply.github.com>
2024-11-16 03:28:32 +0700

    Update login.php

--- login_api/login.php
+++ login_api/login.php
@@ -1,10 +1,11 @@
 <?php
 header('Content-Type: application/json'); // Đặt Content-Type là JSON
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "your_database_name";
+$servername = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
 
 // Kết nối đến cơ sở dữ liệu
 $conn = new mysqli($servername, $username, $password, $dbname);
