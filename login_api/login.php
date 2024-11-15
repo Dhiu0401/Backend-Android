51d19fe267c9977b7616d55b414765180e02cd4c
Dhiu0401 <165197217+Dhiu0401@users.noreply.github.com>
2024-11-16 03:10:15 +0700

    Update login.php

--- login_api/login.php
+++ login_api/login.php
@@ -1,24 +1,17 @@
 <?php
-// Đặt header JSON
-header('Content-Type: application/json');
+header('Content-Type: application/json'); // Đặt Content-Type là JSON
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "your_database_name";
 
-// Lấy thông tin kết nối từ biến môi trường
-$servername = getenv('mysql.railway.internal');      // MYSQLHOST được cung cấp bởi Railway
-$username = getenv('root');        // MYSQLUSER được cung cấp bởi Railway
-$password = getenv('MYSQjRAXNLLTnyuajDFADdSfgsWBvdPSDgHfLPASSWORD');    // MYSQLPASSWORD được cung cấp bởi Railway
-$dbname = getenv('railway');      // MYSQLDATABASE được cung cấp bởi Railway
-$port = getenv('3306');            // MYSQLPORT được cung cấp bởi Railway
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// Kiểm tra kết nối
 if ($conn->connect_error) {
     echo json_encode(["status" => "fail", "message" => "Database connection failed"]);
     exit();
 }
 
-// Nhận email và password từ yêu cầu POST
 $email = $_POST['email'];
 $password = $_POST['password'];
 
@@ -32,6 +25,5 @@ if ($result->num_rows > 0) {
     echo json_encode(["status" => "fail", "message" => "Invalid email or password"]);
 }
 
-// Đóng kết nối
 $conn->close();
 ?>
